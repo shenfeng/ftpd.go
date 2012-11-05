949bed41c5b5990ad12bcbc3af921930f4916b6c
feng <feng.shen@onycloud.com>
2012-11-05 08:57:32 +0800

    try to impl binary search tree

---
+++ tree.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"fmt"
+	// "math/rand"
+)
+
+// A Tree is a binary tree with integer values.
+type Tree struct {
+	Left  *Tree
+	Value int
+	Right *Tree
+}
+
+// New returns a new, random binary tree holding the values k, 2k, ..., 10k.
+func New(k int) *Tree {
+	var t *Tree
+	for _, v := range []int{6, 4, 5, 2, 9, 8, 7, 3, 1} {
+		t = insert(t, (1+v)*k)
+	}
+	return t
+}
+
+func insert(t *Tree, v int) *Tree {
+	if t == nil {
+		return &Tree{nil, v, nil}
+	}
+	if v < t.Value {
+		t.Left = insert(t.Left, v)
+	} else {
+		t.Right = insert(t.Right, v)
+	}
+	return t
+}
+
+func (t *Tree) String() string {
+	if t == nil {
+		return "()"
+	}
+	s := ""
+	if t.Left != nil {
+		s += t.Left.String() + " "
+	}
+	s += fmt.Sprint(t.Value)
+	if t.Right != nil {
+		s += " " + t.Right.String()
+	}
+	return "(" + s + ")"
+}
+
+type Queue struct {
+	nodes []*Tree
+	head  int
+	tail  int
+	count int
+}
+
+// Push adds a node to the queue.
+func (q *Queue) Push(n *Tree) {
+	if q.head == q.tail && q.count > 0 {
+		nodes := make([]*Tree, len(q.nodes)*2)
+		copy(nodes, q.nodes[q.head:])
+		copy(nodes[len(q.nodes)-q.head:], q.nodes[:q.head])
+		q.head = 0
+		q.tail = len(q.nodes)
+		q.nodes = nodes
+	}
+	q.nodes[q.tail] = n
+	q.tail = (q.tail + 1) % len(q.nodes)
+	q.count++
+}
+
+// Pop removes and returns a node from the queue in first to last order.
+func (q *Queue) Pop() *Tree {
+	if q.count == 0 {
+		return nil
+	}
+	node := q.nodes[q.head]
+	q.head = (q.head + 1) % len(q.nodes)
+	q.count--
+	return node
+}
+
+// Stack is a basic LIFO stack that resizes as needed.
+type Stack struct {
+	nodes []*Tree
+	count int
+}
+
+// Push adds a node to the stack.
+func (s *Stack) Push(n *Tree) {
+	if s.count >= len(s.nodes) {
+		nodes := make([]*Tree, len(s.nodes)*2)
+		copy(nodes, s.nodes)
+		s.nodes = nodes
+	}
+	s.nodes[s.count] = n
+	s.count++
+}
+
+// Pop removes and returns a node from the stack in last to first order.
+func (s *Stack) Pop() *Tree {
+	if s.count == 0 {
+		return nil
+	}
+	node := s.nodes[s.count-1]
+	s.count--
+	return node
+}
+
+func (t *Tree) preOrder() {
+	s := &Stack{nodes: make([]*Tree, 3)}
+	if t != nil || s.count > 0 {
+		if t != nil {
+			fmt.Println(t.Value)
+			s.Push(t)
+			t = t.Left
+		} else {
+			t = s.Pop().Right
+		}
+	}
+}
+
+func main() {
+	t := New(1)
+	fmt.Println(t)
+	t.preOrder()
+}
